cdbeafed6a3b7554173f364162cd4c45c1b6657b
Gustavo Machado <78886228+gustavosm1@users.noreply.github.com>
2021-10-24 00:48:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: 'gustavosm1',
+  youtube: 'channel/UC8ChgnmmpKBoqP7_2RgbAAw',
+  twitter: 'gustavomachad_',
+  instagram: 'g_smachado'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
